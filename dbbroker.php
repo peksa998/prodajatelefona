6bdd47f8a753c687a2697e3de5517786c5fc1219
peksa998 <44735425+peksa998@users.noreply.github.com>
2023-01-11 20:46:50 +0100

    Update dbbroker.php

--- dbbroker.php
+++ dbbroker.php
@@ -1,8 +1,8 @@
 <?php
 
-$hostname = "localhost";
-$username = "root";
-$password = "";
+$hostname = "prodajatelefonav3-server.mysql.database.azure.com";
+$username = "pbbvqubjjr";
+$password = "1688V6FDU0VH7T52$";
 $database = "prodajamobilnihtelefona";
  
 $conn = mysqli_connect($hostname,$username,$password,$database) or die("Database connection failed");
